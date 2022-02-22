3630040b5a65ca1de0a61900ce33c066eb249ce9
lael aichaoui <lael.aichaoui@gmail.com>
2022-02-22 08:16:50 -0800

    strikers routes created

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  ressources :strikers, only: [:index, :show, :new, :create, :destroy]
+  get "/my-strikers", to: "strikers#my_strikers"
 end

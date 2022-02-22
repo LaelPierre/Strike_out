4e735f8471bdd555c66d96f52a809ff3c4da936b
Freddy Abelard <abelard.freddy@gmail.com>
2022-02-22 17:47:34 -0500

    Create the controller and Implement the new and create method with the view new(form)

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,34 @@
+class BookingsController < ApplicationController
+  before_action :set_booking, only: [:update]
+  before_action :set_striker, only: [:new, :create]
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.client = current_user
+    @booking.striker = @striker
+    @booking.duration = (@booking.end_date - @booking.start_date).to_i
+    @booking.price = @striker.price * @booking.duration
+    if @booking.save
+      redirect_to my_bookings_path, notice: 'Booking created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :renter_id)
+  end
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  def set_striker
+    @striker = Striker.find(params[:striker_id])
+  end
+end

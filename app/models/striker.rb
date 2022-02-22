0c68fc0fd406909ad93f3e84b933bc1a7af12f1b
Freddy Abelard <abelard.freddy@gmail.com>
2022-02-22 11:28:05 -0500

    Created the association and validation for sriker and booking

--- app/models/striker.rb
+++ app/models/striker.rb
@@ -1,3 +1,6 @@
 class Striker < ApplicationRecord
   belongs_to :renter, class_name: 'User', foreign_key: :renter_id
+
+  validates :description, :price, :war_tag, presence: true
+  validates :war_tag, uniqueness: true
 end

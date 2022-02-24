35a721c3a685564fa54d4c185ebc2ce15283e054
Dan Amar <danamar2001@gmail.com>
2022-02-24 17:45:41 -0500

    got started on card layout

--- app/controllers/strikers_controller.rb
+++ app/controllers/strikers_controller.rb
@@ -32,7 +32,7 @@ class StrikersController < ApplicationController
   end
 
   def my_strikers
-    @striker = Striker.where(params[:renter_id])
+    @strikers = Striker.where(params[:renter_id])
   end
 
   def destroy

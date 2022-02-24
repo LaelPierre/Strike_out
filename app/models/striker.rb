653a2169830a809c8accb6b0eef2aff0fdf1d3ed
Freddy Abelard <abelard.freddy@gmail.com>
2022-02-24 11:31:35 -0500

    Change the name of lat and long in striker and set the mapbox config

--- app/models/striker.rb
+++ app/models/striker.rb
@@ -1,5 +1,7 @@
 class Striker < ApplicationRecord
   belongs_to :renter, class_name: 'User', foreign_key: :renter_id
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 
   validates :description, :price, :war_tag, presence: true
   validates :war_tag, uniqueness: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_23_172639) do
+ActiveRecord::Schema.define(version: 2022_02_24_162639) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,8 +36,8 @@ ActiveRecord::Schema.define(version: 2022_02_23_172639) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "address"
-    t.float "lat"
-    t.float "long"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["renter_id"], name: "index_strikers_on_renter_id"
   end
 
